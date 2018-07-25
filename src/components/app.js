1d8284821e3bffde4cc7f1adde6e7d980993450e
Giorgi Jangirashvili <jangirashvili.george@gmail.com>
2018-07-25 13:36:26 +0400

    brief code changes

--- src/components/app.js
+++ src/components/app.js
@@ -1,86 +1,97 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import {fetchWeather} from '../actions/index';
-import WeatherList from '../Container/weather_list';
-import {fetchForecast} from '../actions/forecast';
-import ForecastList from '../Container/forecast_list';
-import {fetchDaily} from '../actions/daily';
-import DailyList from '../Container/daily_list';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import { bindActionCreators } from "redux"
+import { fetchWeather } from "../actions/index"
+import WeatherList from "../Container/WeatherList"
+import { fetchForecast } from "../actions/forecast"
+import ForecastList from "../Container/ForecastList"
+import { fetchDaily } from "../actions/daily"
+import DailyList from "../Container/DailyList"
+import { NavLink } from "react-router-dom"
 
 export class App extends Component {
+  state = { city: "Choose a city to view the weather" }
 
-  constructor(props){
-    super(props);
+  handleClick = event => this.setState({ city: event.target.textContent })
 
-    this.state = {city:'Choose a city to view the weather'};
-    this.handleClick = this.handleClick.bind(this);
+  componentDidUpdate() {
+    this.props.fetchWeather(this.state.city)
+    this.props.fetchForecast(this.state.city)
+    this.props.fetchDaily(this.state.city)
   }
 
-  
-
-  
   render() {
-
-    let cities = ['Tbilisi', 'Zugdidi', 'Kutaisi','Batumi','Gori','Poti','Mtskheta','Dusheti','Kobuleti'];
-    
+    const cities = [
+      "Tbilisi",
+      "Zugdidi",
+      "Kutaisi",
+      "Batumi",
+      "Gori",
+      "Poti",
+      "Mtskheta",
+      "Dusheti",
+      "Kobuleti"
+    ]
 
     return (
-      <div >
+      <div>
         <div className="navbar" id="navbar">
           <nav className="navbar navbar-lg navbar-light bg-blue">
-              <a className="navbar-brand" href="#"><img src="/src/icons/rainbow.png" width="40" height="40" className="d-inline-block align-top" alt=""/>
-                Weather</a>
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" >
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                  {cities.map(city => <a className="nav-item nav-link" href="#" onClick={this.handleClick} key={_.random(0,999999)}>{city}</a>)}
-                </div>
+            <a className="navbar-brand" href="#">
+              <img
+                src="/src/icons/rainbow.png"
+                width="40"
+                height="40"
+                className="d-inline-block align-top"
+                alt=""
+              />
+              Weather
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarNavAltMarkup"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+                {cities.map(city => (
+                  <a
+                    className="nav-item nav-link"
+                    href="#"
+                    onClick={this.handleClick}
+                    key={_.random(0, 999999)}
+                  >
+                    {city}
+                  </a>
+                ))}
               </div>
-              
-            </nav>
-            <div className="showdata">
-              <WeatherList cityName = {this.state.city} /> 
-              {/* <ForecastList cityName = {this.state.city} /> */}
-              <ForecastList  />
-              <DailyList />
             </div>
-            
-            <NavLink to="/weathermap"> Map </NavLink>
-            </div>
-            
-            
-            
+          </nav>
+          <div className="showdata">
+            <WeatherList cityName={this.state.city} />
+            {/* <ForecastList cityName = {this.state.city} /> */}
+            <ForecastList />
+            <DailyList />
+          </div>
 
+          <NavLink to="/weathermap"> Map </NavLink>
+        </div>
       </div>
-
-      
-    );
+    )
   }
-  
-
-  handleClick(event){
-    this.setState({city: event.target.textContent});
-  }
-
-  componentDidUpdate(){
-
-      this.props.fetchWeather(this.state.city);
-      this.props.fetchForecast(this.state.city);
-      this.props.fetchDaily(this.state.city);
-
-
-  }
-
-
- 
 }
 
-function mapDispatchToProps(dispatch){
-  return bindActionCreators({fetchWeather , fetchForecast, fetchDaily}, dispatch);
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    { fetchWeather, fetchForecast, fetchDaily },
+    dispatch
+  )
 }
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(
+  null,
+  mapDispatchToProps
+)(App)
--- src/components/map.js
+++ src/components/map.js
@@ -1,41 +1,42 @@
-import React, {Component} from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
- 
+import React, { Component } from "react"
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react"
+
 const style = {
-  width: '80%',
-  height: '80%',
-  margin: '5%'
+  width: "80%",
+  height: "80%",
+  margin: "5%"
 }
 
 export class MapContainer extends Component {
-  
-
   render() {
     return (
-      <Map google={this.props.google} 
-            zoom={8}
-            style={style}
-            initialCenter={{
-            lat: 42.090952,
-            lng: 43.598390
-          }}>
- 
-        <Marker onClick={this.onMarkerClick}
-                name={'Tbilisi'}
-                icon={{
-                    url: "/src/icons/01d.svg",
-                    anchor: new google.maps.Point(32,32),
-                    scaledSize: new google.maps.Size(64,64)}}
-                position = {{lat: 41.6935,
-                  lng: 44.8015}}
-                     />
- 
-        
+      <Map
+        google={this.props.google}
+        zoom={8}
+        style={style}
+        initialCenter={{
+          lat: 42.090952,
+          lng: 43.59839
+        }}
+      >
+        <Marker
+          onClick={this.onMarkerClick}
+          name={"Tbilisi"}
+          icon={{
+            url: "/src/icons/01d.svg",
+            anchor: new google.maps.Point(32, 32),
+            scaledSize: new google.maps.Size(64, 64)
+          }}
+          position={{
+            lat: 41.6935,
+            lng: 44.8015
+          }}
+        />
       </Map>
-    );
+    )
   }
 }
- 
+
 export default GoogleApiWrapper({
-  apiKey: ('AIzaSyD0XNO0tPBzLZtOn4fKDWPeJ3q_2BoeQbo')
+  apiKey: "AIzaSyD0XNO0tPBzLZtOn4fKDWPeJ3q_2BoeQbo"
 })(MapContainer)
